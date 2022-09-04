c74fa4851deee6438053d71d4f4aa4c930f3e758
Haley Seymour <haleycseymour@comcast.net>
2022-09-04 13:18:21 -0700

    add a song method

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
-const { start } = require("repl");
-const { finished } = require("stream");
 require("dotenv").config();
 
 const db = mysql.createConnection(
@@ -49,8 +47,81 @@ function menu() {
 
 addASong = async () => {
   try {
-    console.log("chose to add a song");
-    menu();
+    console.log("You chose to add a song.");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "song_name",
+        message: "What is the name of the song?",
+        validate: (ans) => {
+            if (ans.trim() !== ''){
+                return true
+            } 
+            return 'Please enter the song name'
+        }
+      },
+      {
+        type: "input",
+        name: "artist",
+        message: "Who is the artist?",
+        validate: (ans) => {
+            if (ans.trim() !== ''){
+                return true
+            } 
+            return 'Please enter the artist'
+        }
+      },
+      {
+        type: "input",
+        name: "genre",
+        message: "What is the genre?",
+        validate: (ans) => {
+            if (ans.trim() !== ''){
+                return true
+            } 
+            return 'Please enter the genre'
+        }
+      },
+      {
+        type: "input",
+        name: "user_name",
+        message: "What is your name?",
+        validate: (ans) => {
+            if (ans.trim() !== ''){
+                return true
+            } 
+            return 'Please enter your name'
+        }
+      },
+    ]).then(answers => {
+        db.query(`SELECT song_name FROM song`, function (error, results) {
+            if (error) {
+                throw error
+            } else {
+                let match = results.filter(result => result.song_name.toLowerCase() == answers.song_name.toLowerCase())
+                if (match.length != 0) {
+                    console.log('Song already exists!');
+                    return menu();
+                } else {
+                    let song = answers.song_name.trim();
+                    let artist = answers.artist.trim();
+                    let genre  = answers.genre.trim();
+                    let user = answers.user_name.trim();
+
+                    // TODO: Add in user association if user exists or create new user 
+                    console.log(`Song: ${song}, Artist: ${artist}, Genre: ${genre}, User: ${user}`)
+                    db.query(`INSERT INTO song (song_name, artist, genre) VALUES (?, ?)`, [song, artist, genre], function (err, results) {
+                        if (err) {
+                            throw err
+                        } else {
+                            console.log(`Success! ${answers.song_name.trim()} was added!`);
+                            return menu();
+                        }
+                    })
+                }
+            }
+        })
+    })
   } catch (err) {
     console.log(err);
   }
@@ -58,24 +129,26 @@ addASong = async () => {
 
 viewAllSongs = async () => {
   try {
-    console.log("chose to view all songs");
-    db.query(`SELECT 
+    console.log("You chose to view all songs.");
+    db.query(
+      `SELECT 
         song_name as 'song', 
         artist, 
         genre, 
         user.name as 'picked by' 
         FROM song 
         LEFT JOIN user 
-        ON song.user_id = user.id`, 
-        function (err, results) {
+        ON song.user_id = user.id`,
+      function (err, results) {
         if (err) {
-            throw err
+          throw err;
         } else {
-            const table = cTable.getTable(results);
-            console.log(table);
-            menu();
+          const table = cTable.getTable(results);
+          console.log(table);
+          return menu();
         }
-    })
+      }
+    );
   } catch (err) {
     console.log(err);
   }
